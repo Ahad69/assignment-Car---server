53a6c2ecee2b9867fee332d7e612b0b74ddcb0d5
Ahad hossain <ahadxx99@gmail.com>
2022-04-29 04:05:32 -0500

    database connected

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000 ;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+// database 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxcn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const itemsCollections = client.db("Inventory").collection('Items');
+      
+      // get all items 
+      app.get('/items' , async(req , res)=>{
+          const query = {};
+          const result = await itemsCollections.find(query).toArray();
+          res.send(result)
+      });
+
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+app.get('/' , (req , res)=>{
+    res.send('Assignment Server is running')
+});
+
+app.listen(port , ()=>{
+    console.log("port also running" , port)
+})
