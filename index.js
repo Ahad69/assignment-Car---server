721a72ed01e7d69a1c4638669a9bf4ab782f1946
Ahad hossain <ahadxx99@gmail.com>
2022-04-29 05:50:46 -0500

    details by id

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000 ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-// middleware 
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+// middleware
 app.use(cors());
 app.use(express.json());
 
-
-// database 
+// database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxcn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run() {
-    try {
-      await client.connect();
-      const itemsCollections = client.db("Inventory").collection('Items');
-      
-      // get all items 
-      app.get('/items' , async(req , res)=>{
-          const query = {};
-          const result = await itemsCollections.find(query).toArray();
-          res.send(result)
-      });
-
-
-    } finally {
-    //   await client.close();
-    }
-  }
-  run().catch(console.dir);
-
-
-
-app.get('/' , (req , res)=>{
-    res.send('Assignment Server is running')
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port , ()=>{
-    console.log("port also running" , port)
-})
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollections = client.db("Inventory").collection("Items");
+
+    // get all items
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const result = await itemsCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await itemsCollections.findOne(query);
+      res.send(result);
+    });
+
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Assignment Server is running");
+});
+
+app.listen(port, () => {
+  console.log("port also running", port);
+});
