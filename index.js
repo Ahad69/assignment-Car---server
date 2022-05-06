2efa6056ad8980b778c8b0d5522424a6bb523d07
Ahad hossain <ahadxx99@gmail.com>
2022-05-06 06:47:02 -0500

    trying

--- index.js
+++ index.js
@@ -13,22 +13,20 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-function verifyToken(req , res , next){
-  const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message : 'unauthorised'})
-  }
-  const token = authHeader.split(' ')[1]
-  jwt.verify(token , process.env.TOKEN , (err , decoded)=>{
-    if(err){
-      return res.status(403).send({message : 'Forbidden'})
-    }
-   
-   
-    req.decoded = decoded;
-  })
-  next()
-}
+// function verifyToken(req , res , next){
+//   const authHeader = req.headers.authorization;
+//   if(!authHeader){
+//     return res.status(401).send({message : 'unauthorised'})
+//   }
+//   const token = authHeader.split(' ')[1]
+//   jwt.verify(token , process.env.TOKEN , (err , decoded)=>{
+//     if(err){
+//       return res.status(403).send({message : 'Forbidden'})
+//     }
+//     req.decoded = decoded;
+//   })
+//   next()
+// }
 
 // database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mxcn0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -61,14 +59,18 @@ async function run() {
     });
 
     // get item by email 
-    app.get('/my-items' , verifyToken ,  async(req , res)=>{
-      const decodedEmail = req.decoded.email
+    app.get('/my-items'  ,  async(req , res)=>{
+      // const decodedEmail = req.decoded.email
       const email = req.query.email;
-      if(email === decodedEmail){
-        const query = {email}
-        const result = await itemsCollections.find(query).toArray()
-        res.send(result)
-      }
+      const query = {email}
+      const result = await itemsCollections.find(query).toArray()
+      res.send(result)
+
+      // if(email === decodedEmail){
+      //   const query = {email}
+      //   const result = await itemsCollections.find(query).toArray()
+      //   res.send(result)
+      // }
       
     })
 
